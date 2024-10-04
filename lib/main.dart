335e82f9bb1559f582ab40138012b9b8e4772241
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2024-10-04 20:04:03 +0700

    Dialog Widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,58 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // Widget ini adalah root dari aplikasi kamu.
-  @override
+   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+    return const MaterialApp(
+      home: Scaffold(
+        body: MyLayout(),
       ),
-      home: const MyHomePage(title: 'My Increment App'),
     );
   }
+}
+
+class MyLayout extends StatelessWidget {
+  const MyLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        child: const Text('Show alert'),
+        onPressed: () {
+          showAlertDialog(context);
+        },
+      ),
+    );
+  }
+}
+
+showAlertDialog(BuildContext context) {
+  // set up the button
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {
+      Navigator.pop(context);
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: const Text("My title"),
+    content: const Text("This is my message."),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
 
 class MyHomePage extends StatefulWidget {
